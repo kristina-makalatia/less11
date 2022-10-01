2b013077877ec4809e58ccceb29521573a22f77a
kristina-makalatia <kristina.makalatia@geolab.edu.ge>
2022-10-01 13:29:58 +0400

    add grid

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+//variables
+let userName = "giorgi"; //string
+let userAge = 25; //number
+
+let x = "15";
+let y = 10;
+console.log(x + y);
+
+//array
+let userNames = ["giorgi", "levani", "beka", "davit", "sopo"];
+// 0         1        2       3           4
+
+//loop
+for (let item of userNames) {
+  console.log(item);
+}
